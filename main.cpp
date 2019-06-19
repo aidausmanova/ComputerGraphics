b285b063fc948a70d8bbe392b23589e63864be5c
Aida Usmanova <aida.ct127@gmail.com>
2019-06-19 21:38:05 +0900

    First: line & triangle, Second: square & teapot, Third: polygons, Fourth: circles
    First: line & triangle, Second: square & teapot, Third: polygons, Fourth: circles

--- main.cpp
+++ main.cpp
@@ -1,66 +1,100 @@
 #include <OpenGL/OpenGL.h>
 #include <GLUT/GLUT.h>
+#include <math.h>
+#include <stdlib.h>
 
-void square(void){
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+const double TWO_PI = 0.0 - 6.2831853;
+GLsizei width = 400, height = 400;
+
+GLuint regHex;
+
+class screenPt{
+private: GLint x, y;
     
-    glBegin(GL_QUADS);
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.5, -0.5);
-        glVertex2f(0.5, 0.5);
-        glVertex2f(-0.5, 0.5);
-    glEnd();
-     
-    /*
-    glBegin (GL_LINE_STRIP);
-     glColor3f(1.0, 0.0, 0.0);
-    glVertex2f(200, 100);
-    glVertex2f(100, 200);
-    glVertex2f(120, 50);
-    glVertex2f(150, 200);
-    glVertex2f(50, 100);
-    glEnd ( );
-    */
-    glFlush();
-}
+public:
+    screenPt(){
+        x = y = 0;
+    }
+    void setCoor (GLint xC, GLint yC){
+        x = xC;
+        y = yC;
+    }
+    GLint getx() const{
+        return x;
+    }
+    GLint gety() const{
+        return y;
+    }
+    void incrX(){
+        x++;
+    }
+    void decrY(){
+        y--;
+    }
+};
 
-void teapot(void){
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0, 0.0, 1.0);
-    glutWireTeapot(0.5);
-    //glutSolidTeapot(.5);
-    glFlush();
-}
-
-void points(void){
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    //glClear(GL_COLOR_BUFFER_BIT);
-    
+void setPixel (GLint xC, GLint yC){
     glBegin(GL_POINTS);
-        glColor3f(1.0, 1.0, 1.0);
-        glVertex2i(50, 100);
-        glVertex2i(75, 150);
-        glVertex2i(100, 200);
+    glVertex2i(xC, yC);
+    glEnd();
+}
+
+void circleMidpoint(GLint xc, GLint yc, GLint radius){
+    screenPt circPt;
+    GLint p = 1 - radius;
+    circPt.setCoor(0, radius);
+    void circlePlotPoints(GLint, GLint, screenPt);
+    circlePlotPoints(xc, yc, circPt);
+    while (circPt.getx()<circPt.gety()) {
+        circPt.incrX();
+        if(p<0)
+            p += 2*circPt.getx()+1;
+        else{
+            circPt.decrY();
+            p += 2*(circPt.getx()-circPt.gety())+1;
+        }
+        circlePlotPoints(xc, yc, circPt);
+    }
+}
+
+void circlePlotPoints(GLint xc, GLint yc, screenPt circPt){
+    setPixel(xc + circPt.getx(), yc + circPt.gety());
+    setPixel(xc - circPt.getx(), yc + circPt.gety());
+    setPixel(xc + circPt.getx(), yc - circPt.gety());
+    setPixel(xc - circPt.getx(), yc - circPt.gety());
+    setPixel(xc + circPt.gety(), yc + circPt.getx());
+    setPixel(xc - circPt.gety(), yc + circPt.getx());
+    setPixel(xc + circPt.gety(), yc - circPt.getx());
+    setPixel(xc - circPt.gety(), yc - circPt.getx());
+}
+
+void init(){
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, 400.0, 0.0, 400.0);
+}
+
+void printCircle(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glColor3f(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+    circleMidpoint(200, 200, 90);
+    circleMidpoint(100, 100, 40);
+    circleMidpoint(100, 300, 40);
+    circleMidpoint(300, 100, 40);
+    circleMidpoint(300, 300, 40);
     glEnd();
     glFlush();
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv){
     glutInit(&argc, argv);
-    glutInitWindowSize(300, 300);
-    glutCreateWindow("Square");
-    glutDisplayFunc(square);
-    
-    glutInitWindowPosition(400, 50);
-    glutCreateWindow("Teapot");
-    glutDisplayFunc(teapot);
-    
-    glutInitWindowPosition(50, 400);
-    glutCreateWindow("Points");
-    glutDisplayFunc(points);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("Circles");
+    init();
+    glutDisplayFunc(printCircle);
     glutMainLoop();
     return 0;
 }
