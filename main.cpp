98b10b267f69cf09feec1c08eec930a3654cf217
Aida Usmanova <aida.ct127@gmail.com>
2019-06-19 21:33:22 +0900

    Square and Teapot
    Square and Teapot

--- main.cpp
+++ main.cpp
@@ -1,44 +1,66 @@
 #include <OpenGL/OpenGL.h>
 #include <GLUT/GLUT.h>
 
-void init (void){
-    glClearColor (1.0, 1.0, 1.0, 0.0); // Set display-window color to white.
-    glMatrixMode (GL_PROJECTION); // Set projection parameters.
-    gluOrtho2D (0.0, 200.0, 0.0, 150.0);
-}
-
-void lineSegment (void){
-    glClear (GL_COLOR_BUFFER_BIT); // Clear display window.
-    glColor3f (0.0, 0.4, 0.2); // Set line segment color to green.
-    glBegin (GL_LINES);
-    glVertex2i (180, 15); // Specify line-segment geometry.
-    glVertex2i (10, 145);
-    glEnd ( );
-    glFlush ( ); // Process all OpenGL routines as quickly as possible.
-}
-
-void polygon(void){
+void square(void){
+    glClearColor(1.0, 1.0, 1.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex2i(50, 50);
-    glVertex2i(150, 50);
-    glVertex2i(150, 120);
+    
+    glBegin(GL_QUADS);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(0.5, -0.5);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(-0.5, 0.5);
+    glEnd();
+     
+    /*
+    glBegin (GL_LINE_STRIP);
+     glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(200, 100);
+    glVertex2f(100, 200);
+    glVertex2f(120, 50);
+    glVertex2f(150, 200);
+    glVertex2f(50, 100);
+    glEnd ( );
+    */
+    glFlush();
+}
+
+void teapot(void){
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.0, 1.0);
+    glutWireTeapot(0.5);
+    //glutSolidTeapot(.5);
+    glFlush();
+}
+
+void points(void){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    //glClear(GL_COLOR_BUFFER_BIT);
+    
+    glBegin(GL_POINTS);
+        glColor3f(1.0, 1.0, 1.0);
+        glVertex2i(50, 100);
+        glVertex2i(75, 150);
+        glVertex2i(100, 200);
     glEnd();
     glFlush();
 }
 
-int main (int argc, char **argv){
-    glutInit (&argc, argv); // Initialize GLUT.
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB); // Set display mode.
-    glutInitWindowPosition (50, 70); // Set top-left display-window position.
-    glutInitWindowSize (400, 300); // Set display-window width and height.
-    glutCreateWindow ("First1");
-    init();
-    glutDisplayFunc(lineSegment);
-    glutInitWindowPosition (50, 420);
-    glutCreateWindow ("First2"); // Create display window.
-    init ( ); // Execute initialization procedure.
-    glutDisplayFunc (polygon); // Send graphics to display window.
-    glutMainLoop ( ); // Display everything and wait.
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(300, 300);
+    glutCreateWindow("Square");
+    glutDisplayFunc(square);
+    
+    glutInitWindowPosition(400, 50);
+    glutCreateWindow("Teapot");
+    glutDisplayFunc(teapot);
+    
+    glutInitWindowPosition(50, 400);
+    glutCreateWindow("Points");
+    glutDisplayFunc(points);
+    glutMainLoop();
+    return 0;
 }
